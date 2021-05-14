d3f371f807e83a73a49924b01bb58f3e2e305d17
qkrrjs03 <80082533+qkrrjs03@users.noreply.github.com>
2021-05-14 11:58:13 +0900

    5/14 배경 플레이어

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+
+    public float tspeed = 13f;
+    public float rspeed = 130f;
+    void Update()
+    {
+        translation = Input.GetAxis("Ve") * tspeed;
+        rotation = Input.GetAxis("Ho") * rspeed;
+
+        transform.Translate(0, 0,translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
